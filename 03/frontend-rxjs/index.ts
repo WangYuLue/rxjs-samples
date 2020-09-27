436cbe212951d2dc9358a8b315e4e0384682d3fa
wangyulue <1206180097@qq.com>
2020-09-27 15:18:48 +0800

    feat(*):demo03 code optimize

--- 03/frontend-rxjs/index.ts
+++ 03/frontend-rxjs/index.ts
@@ -1,29 +1,20 @@
 import { fromEvent } from 'rxjs';
 import { tap, map, switchMap, takeUntil } from 'rxjs/operators';
 
-function getTranslate(element) {
-  const style = getComputedStyle(element)
-  const regExp = /matrix\((\d+,\s){4}(\d+),\s(\d+)/i
-  const result = style.transform.match(regExp)
-  if (result) {
-    return {
-      x: parseInt(result[2], 10),
-      y: parseInt(result[3], 10)
-    }
-  } else {
-    return {
-      x: 0,
-      y: 0
-    }
-  }
+function getTranslate(dom: HTMLElement): { top: number, left: number } {
+  const { top, left } = dom.getBoundingClientRect()
+  return { top, left }
 }
 
-function setTranslate(element, pos) {
-  element.style.transform = `translate(${pos.x}px, ${pos.y}px)`
+function setTranslate(element: HTMLElement, pos: { top: number, left: number }) {
+  const { top, left } = pos;
+  element.style.top = `${top}px`;
+  element.style.left = `${left}px`;
 }
 
 const init = () => {
   const box = document.getElementById('box');
+
   if (box) {
     const mouseDown$ = fromEvent(box, 'mousedown');
     const mouseMove$ = fromEvent(document, 'mousemove');
@@ -31,20 +22,25 @@ const init = () => {
 
     mouseDown$.pipe(
       map(event => {
+        const { clientX, clientY } = event as MouseEvent
         return {
-          pos: getTranslate(box),
-          event
+          boxPositon: getTranslate(box),
+          mousePosition: {
+            top: clientY,
+            left: clientX
+          }
         }
       }),
-      switchMap(state => {
-        const initialPos = state.pos
-        const e = state.event
-        const { clientX, clientY } = e as MouseEvent;
+      switchMap(position => {
+        const { boxPositon, mousePosition } = position
         return mouseMove$.pipe(
-          map(moveEvent => ({
-            x: (moveEvent as MouseEvent).clientX - clientX + initialPos.x,
-            y: (moveEvent as MouseEvent).clientY - clientY + initialPos.y,
-          })),
+          map(event => {
+            const { clientX, clientY } = event as MouseEvent
+            return {
+              left: clientX - mousePosition.left + boxPositon.left,
+              top: clientY - mousePosition.top + boxPositon.top
+            }
+          }),
           takeUntil(mouseUp$)
         )
       })
